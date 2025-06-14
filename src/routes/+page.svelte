cb957afe44df213f53926dfec4ecde31b94da173
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-06-14 09:19:29 +0600

    Refactor Header component to utilize props and enhance binding examples in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,23 @@
 <script>
 	import Header from "./Header.svelte";
+
+    // let name = 'Su';
+    let name = $state('su');
 </script>
-<Header/>
+
+<!-- pass the props `name="Munna"` from here to other component Header.svelte -->
+<Header name="Munna"/>
+
+<!-- Initialize the variable `let name = 'Su'` and use it in side the own component without passing props -->
+ 
+<!-- <Header name={name}/> -->
+
+<Header {name}/> 
+
+<!-- this both will same `name={name} & {name}` , show the same result. -->
+
+
+<!-- bind values = help to change the value across everywhere  --> 
+Example : <input type='text' placeholder="TypeAnything" bind:value={name}>
+
+<!-- In React if we want to change anything we had to use onchange but here simple "bind:value={props_name}" -->
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-
+let {name} : {name:string} = $props();
 </script>
 
 <h1>
-    Hello From Header
+    Hello From {name} <br>  <!-- Hello From Munna -->
+
+    <!-- Simple Javascript boolean Check the output for the result -->
+    Hello From {name == 'X'} <br> <!-- Hello From false  -->
+    Hello From {name == 'Munna'} <!-- Hello From true -->
 </h1>
+
+<h2>
+    <!-- We can use javascript here without any hesitation -->
+     {name.replace('M', 'm')} <br> <!--change Munna in to munna  -->
+     {name.replaceAll('n', 'm')}  <!--change Munna in to Mumma  -->
+</h2>
